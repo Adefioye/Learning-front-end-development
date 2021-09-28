f11a68ebd3e3717af9537d4b0d57655e19ded13a
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-09-28 07:36:15 +0100

    finished learning plain JS script

--- Colt-steele-web-bootcamp/javascript-starter/playground-beginning/app.js
+++ Colt-steele-web-bootcamp/javascript-starter/playground-beginning/app.js
@@ -221,17 +221,25 @@
 //     }
 // }
 
-const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+// const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
 
-let total = 0;
-for (let price of prices) {
-    total += price;
+// let total = 0;
+// for (let price of prices) {
+//     total += price;
+// }
+// console.log(total);
+
+// const totalPrice = prices.reduce((acc, price) => {
+//     return acc + price
+// })
+// console.log(totalPrice);
+
+// DEFAULT PARAMETERS
+function rollDie(numSides) {
+    if (numSides === undefined) {
+        numSides = 6;
+    }
+    return Math.floor(Math.random() * numSides) + 1;
 }
-console.log(total);
-
-const totalPrice = prices.reduce((acc, price) => {
-    return acc + price
-})
-console.log(totalPrice);
 
 
