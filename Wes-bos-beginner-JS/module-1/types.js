51100bba569fbbdf8d445c6410171c77b284f6c5
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-09-29 14:58:42 +0100

    finished module 1 on types in JS in wes-bos JS course

---
+++ Wes-bos-beginner-JS/module-1/types.js
@@ -0,0 +1,46 @@
+// const name = "wes";
+// const middle = "topher";
+// const last = `bos`;
+
+// const sentence = 'She\'s so cool \\';
+
+// const sentence = "she's so \"cool\""
+
+// Add multiline string
+
+// Using backslash
+// const song = 'Ohhh! \
+// \
+// ya \
+// \
+// I like \
+// \
+// pizza\
+// '
+
+// console.log(song);
+
+// Using backticks
+// const song = `Ohhh
+
+// ya
+
+// I like
+// pizza`;
+
+// console.log(song);
+
+const name = 'Koko';
+const hello = `hello my name is ${name}. Nice to meet you. I am ${27 + 1} years old'`;
+const html = `
+  <div>
+    <h2>${name}</h2>
+    <p>${hello}</p>
+  </div>
+`;
+
+document.body.innerHTML = html;
+
+
+
+
