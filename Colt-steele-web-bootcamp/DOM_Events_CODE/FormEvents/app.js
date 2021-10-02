07c16f77c6f551f98d4b6a11c71a25aea9f9832b
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-10-02 15:38:30 +0100

    finished working on form events exercise colt-steele web bootcamp

--- Colt-steele-web-bootcamp/DOM_Events_CODE/FormEvents/app.js
+++ Colt-steele-web-bootcamp/DOM_Events_CODE/FormEvents/app.js
@@ -1,25 +1,62 @@
 
 
-const tweetForm = document.querySelector('#tweetForm');
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweetsContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     const usernameInput = tweetForm.elements.username;
+//     const tweetInput = tweetForm.elements.tweet;
+//     addTweet(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// });
+
+// const addTweet = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`)
+//     tweetsContainer.append(newTweet);
+// }
+
+// const tweetForm = document.querySelector("#tweetForm");
+// const tweetContainer = document.querySelector("#tweets")
+// tweetForm.addEventListener("submit", function (e) {
+
+//     e.preventDefault();
+//     const userName = tweetForm.elements.username.value;
+//     const tweet = tweetForm.elements.tweet.value;
+
+//     const newTweet = document.createElement("li");
+//     const bTag = document.createElement("b");
+//     bTag.append(userName);
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`);
+//     tweetContainer.append(newTweet);
+// });
+
+const form = document.querySelector('form');
+const productContainer = document.querySelector("ul");
+form.addEventListener("submit", function (e) {
+
     e.preventDefault();
+    const productInput = form.elements.product;
+    const qtyInput = form.elements.qty;
+    addProductInfo(qtyInput.value, productInput.value);
+    qtyInput.value = "";
+    productInput.value = "";
+})
 
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
-});
-
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
+function addProductInfo (qty, product) {
+    const newProduct = document.createElement("li");
+    newProduct.innerText = `${qty} ${product}`;
+    productContainer.appendChild(newProduct);
 }
 
+
+// ul
+// <li>2 product <li>
