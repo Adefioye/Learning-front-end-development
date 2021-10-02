d7f282c1c436639f7d9f9857beaf2f33efbc09bc
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-10-02 17:20:55 +0100

    finished working on change and input events colt-steele web bootcamp

--- Colt-steele-web-bootcamp/DOM_Events_CODE/ChangeAndInputEvents/app.js
+++ Colt-steele-web-bootcamp/DOM_Events_CODE/ChangeAndInputEvents/app.js
@@ -1,10 +1,24 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
 
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
+// // input.addEventListener('change', function (e) {
+// //     console.log("CASKDJASKJHD")
+// // })
+
+// input.addEventListener('input', function (e) {
+//     h1.innerText = input.value;
 // })
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
+// input.addEventListener("change", function (e) {
+//     console.log("There is a change in input values");
+// })
+input.addEventListener("input", function (e) {
+    if (input.value === '') {
+        h1.innerText = "Enter Your Username";
+    }
+    else {
+        h1.innerText = `Welcome, ${input.value}`;
+    }
+});
