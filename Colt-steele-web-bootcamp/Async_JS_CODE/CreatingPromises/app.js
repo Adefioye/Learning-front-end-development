55972ce04bfa4ba124419f9c87b646e92e482a7d
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-10-14 08:08:25 +0100

    finished learning creating promises, async and await in JS

---
+++ Colt-steele-web-bootcamp/Async_JS_CODE/CreatingPromises/app.js
@@ -0,0 +1,113 @@
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random();
+//         setTimeout(() => {
+//             if (rand < 0.7) {
+//                 resolve('YOUR FAKE DATA HERE');
+//             }
+//             reject('Request Error!');
+//         }, 1000)
+//     })
+// }
+
+// fakeRequest('/dogs/1')
+//     .then((data) => {
+//         console.log("DONE WITH REQUEST!")
+//         console.log('data is:', data)
+//     })
+//     .catch((err) => {
+//         console.log("OH NO!", err)
+//     })
+
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('indigo', 1000, () => {
+//                         delayedColorChange('violet', 1000, () => {
+
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+
+// const delayedColorChange = (color, delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay)
+//     })
+// }
+
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000))
+
+
+// Making a promise
+
+// new Promise((resolve, reject) => {
+//     resolve();
+// });
+
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random();
+//         setTimeout(() => {
+//             if (rand < 0.7){
+//                 resolve("See your fake data");
+//             }
+//             else {
+//                 reject("Request error!");
+//             }
+//         }, 2000)
+//     })
+// };
+
+// fakeRequest("/dogs/1")
+// .then((data) => {
+//     console.log("Done with Request");
+//     console.log("Data is:", data);
+// })
+// .catch((err) => {
+//     console.log("Oh no!!", err);
+// })
+
+// Using the "Promises" to implement delayedColorChange
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+delayedColorChange("red", 2000)
+    .then(() => delayedColorChange("blue", 2000))
+    .then(() => delayedColorChange("olive", 2000))
+    .then(() => delayedColorChange("orange", 2000))
+    .then(() => delayedColorChange("yellow", 2000))
+    .then(() => delayedColorChange("green", 2000))
+    .then(() => delayedColorChange("indigo", 2000))
+    .then(() => delayedColorChange("violet", 2000))
